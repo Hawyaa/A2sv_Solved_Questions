b5264b28cfc73318eddb3c73c834e273a7fb54fa
Hawi <hawi4936@gmail.com>
2026-04-17 12:06:24 -0700

    remove star from string

---
+++ leetcode/2390.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeStars(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack=[]
+        for i in range(len(s)):
+            if s[i]!="*":
+                stack.append(s[i])
+            else:
+                stack.pop()
+        return "".join(stack)
+        
